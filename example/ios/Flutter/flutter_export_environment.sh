5d5ae1eac638fbc3f4ac905e4821cd7d5f05ddee
huma <humazed@gmail.com>
2019-11-09 13:55:37 +0200

    added ios key placeholder in the sample

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/huma/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/huma/AndroidStudioProjects/google_map_location_picker/example"
+export "FLUTTER_TARGET=/Users/huma/AndroidStudioProjects/google_map_location_picker/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/huma/Desktop/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
