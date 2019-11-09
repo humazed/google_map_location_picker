7b1d63130980fd24e48d860e18ab48930a83c788
huma <humazed@gmail.com>
2019-11-09 14:47:21 +0200

    fix problems with ios exampleˆ

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -6,4 +6,6 @@ export "FLUTTER_TARGET=/Users/huma/AndroidStudioProjects/google_map_location_pic
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/huma/Desktop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
 export "TRACK_WIDGET_CREATION=true"
