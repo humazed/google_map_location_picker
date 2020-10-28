a71f8606f9d7147ba93f5186e09343caea6c384a
Almuhannad <henno.noman@gmail.com>
2020-10-28 11:45:46 +0300

    add automaticallyImplyLeading boolean to appbar

--- lib/src/google_map_location_picker.dart
+++ lib/src/google_map_location_picker.dart
@@ -398,6 +398,7 @@ class LocationPickerState extends State<LocationPicker> {
             iconTheme: Theme.of(context).iconTheme,
             elevation: 0,
             backgroundColor: widget.appBarColor,
+            automaticallyImplyLeading: widget.automaticallyImplyLeading,
             key: appBarKey,
             title: SearchInput(
               (input) {
