9fa48d24e677004bdd89ea7723cfd2897cf80d33
davidbeville <bevilledavid@gmail.com>
2021-10-25 18:24:14 +0200

    Update with null safety, android embeddingV2 and Secrets Gradle Plugin for Android

--- example/android/app/src/main/kotlin/com/humazed/google_map_location_picker_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/humazed/google_map_location_picker_example/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.humazed.google_map_location_picker_example
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+
 }
