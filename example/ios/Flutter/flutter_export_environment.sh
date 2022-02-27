c02ffde197f32cb011dba6405b800ae0d467e345
bhargav22 <bhargav@zuzu.in>
2022-02-27 15:25:42 +0530

    package upgrades

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -5,7 +5,6 @@ export "FLUTTER_APPLICATION_PATH=/Users/blb/Development/google_map_location_pick
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1.0.0"
 export "DART_OBFUSCATION=false"
